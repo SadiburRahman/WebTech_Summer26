261af8c8e55421c5473675381521afff6613f0b4
MdSadiburRahman <sadiburrahman125@gmail.com>
2026-07-09 00:51:38 +0600

    Add login and signup forms with data collection functionality; remove outdated practice page

--- WebTech/Login_form/page_1/script_1.js
+++ WebTech/Login_form/page_1/script_1.js
@@ -1,1 +1,22 @@
 console.log("Connected");
+
+function collect_data()
+{
+    collect_email();
+    collect_password();
+    return false;
+}
+
+function collect_email()
+{
+    let email = document.getElementById("email").value;
+    console.log("User's EMAIL: " + email);
+    return false; 
+}
+
+function collect_password()
+{
+    let password = document.getElementById("password").value;
+    console.log("User's PASSWORD: " + password);
+    return false;
+}
---
+++ WebTech/Login_form/page_2/script_2.js
@@ -0,0 +1,68 @@
+function collect_data() 
+{
+    collect_firstname();
+    collect_lastname();
+    collect_email();
+    collect_password();
+    collect_dob();
+    collect_phonenumber();
+    collect_address();
+    // collect_gender();
+    return false;
+}
+
+function collect_firstname()
+{
+    let firstname = document.getElementById("firstname").value;
+    console.log("User's FIRST NAME: " + firstname);
+    return false; 
+}
+
+function collect_lastname()
+{
+    let lastname = document.getElementById("lastname").value;
+    console.log("User's LAST NAME: " + lastname);
+    return false; 
+}
+
+function collect_email()
+{
+    let email = document.getElementById("email").value;
+    console.log("User's EMAIL: " + email);
+    return false; 
+}
+
+function collect_password()
+{
+    let password = document.getElementById("password").value;
+    console.log("User's PASSWORD: " + password);
+    return false;
+}
+
+function collect_dob()
+{
+    let dob = document.getElementById("dob").value;
+    console.log("User's DATE OF BIRTH: " + dob);
+    return false;
+}
+
+function collect_phonenumber()
+{
+    let phonenumber = document.getElementById("phonenumber").value;
+    console.log("User's PHONE NUMBER: " + phonenumber);
+    return false;
+}
+
+function collect_address()
+{
+    let address = document.getElementById("address").value;
+    console.log("User's ADDRESS: " + address);
+    return false;
+}
+
+// function collect_gender()
+// {
+//     let gender = document.querySelector('input[name="gender"]:checked').value;
+//     console.log("User's GENDER: " + gender);
+//     return false;
+// }
